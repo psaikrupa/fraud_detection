9b1a1da057570b2bc2534f31a099aa696151c938
psaikrupa <p.saikrupa@gmail.com>
2023-03-24 11:24:33 +0530

    Added a incorrect sum function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,10 @@
-print("All data processing code will be updated here)
-print("All data processing code will be updated here)
+print("All data processing code will be updated here")
+
+a = 5
+b = 9
+
+sum(a,b):
+    c = a + b
+    return a
+
+print("Added a new function sum which is not correct")
