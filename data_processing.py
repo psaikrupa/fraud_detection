ffdcfdb89a95078f91ca695e436b21bdb7099ffa
psaikrupa <p.saikrupa@gmail.com>
2023-03-24 11:28:14 +0530

    Revert "Added a incorrect sum function"
    This reverts commit 9b1a1da057570b2bc2534f31a099aa696151c938.

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,2 @@
-print("All data processing code will be updated here")
-
-a = 5
-b = 9
-
-sum(a,b):
-    c = a + b
-    return a
-
-print("Added a new function sum which is not correct")
+print("All data processing code will be updated here)
+print("All data processing code will be updated here)
