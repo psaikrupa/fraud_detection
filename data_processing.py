b38af64fb7aa238621bef1bf14a2fd7f76547deb
psaikrupa <p.saikrupa@gmail.com>
2023-03-24 11:55:12 +0530

    Corrected sum function and added multiply function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,14 @@
 print("All data processing code will be updated here)
-print("All data processing code will be updated here)
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a + b
+    return c
+
+def multiply(a,b):
+    c = a * b
+    return c
+
+print("Added a function for sum and multiplication")
